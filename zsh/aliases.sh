88bfc7b5f1ee15f627a367f1ca7637c47da8c1d3
Josh Wilson <josh@clover.com>
2014-09-02 10:36:54 -0700

    Move setup to it's own directory.

---
+++ zsh/aliases.sh
@@ -0,0 +1,11 @@
+alias cdg='cd $(git rev-parse --show-toplevel)'
+
+
+#Clover
+source '/Users/jwils/dotfiles/zsh/clover-aliases.sh'
+
+alias mv="mv -i"
+alias mvim="nocorrect mvim"
+alias vim="mvim -v"
+alias adb="/Applications/Android\ Studio.app/sdk/platform-tools/adb"
+alias mvn="mvn-color"
